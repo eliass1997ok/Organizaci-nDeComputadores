bb90dbc7fdba5296c074ecf354ffc031a731c625
eliass1997ok <gabriel.gaete.l@usach.cl>
2018-08-20 00:28:52 -0300

    LIFO working.

--- Laboratorio #3/Programa/main.c
+++ Laboratorio #3/Programa/main.c
@@ -53,7 +53,24 @@ NumberLinkedList* createNumberList(){
 }
 
 void appendResult(ResultsList* list, int hit, int miss, int associativity, int policy, int** finalState){
+    Results* node;
+    node = (Results*)calloc(1, sizeof(Results));
 
+    node->hit = hit;
+    node->miss = miss;
+    node->associativity = associativity;
+    node->policy = policy;
+    node->finalState = finalState;
+
+    if (list->length == 0){
+        list->first = node;
+        list->last = node;
+    } else {
+        list->last->next = node;
+        list->last = node;
+    }
+
+    list->length++;
 }
 
 void appendNumberList(NumberLinkedList* list, int number){
@@ -92,7 +109,7 @@ void print_usage() {
 }
 
 int searchSpace(int* memory, int large){
-    for (int i=0; i<large; i++) if (! memory[i]) return i;
+    for (int i=0; i<large; i++) if (memory[i] == -1) return i;
 
     return -1;
 }
@@ -155,6 +172,23 @@ int searchInList(NumberLinkedList* list, int number){
     return -1;
 }
 
+int returnPositionArray(NumberLinkedList* list, int positionInList, int* array, int large){
+    NumberNode* node;
+    node = list->first;
+
+    int counter;
+    counter = 0;
+
+    while (counter != positionInList){
+        counter++;
+        node = node->next;
+    }
+
+    for (int i=0; i<large; i++) if (array[i] == node->number) return i;
+
+    return -1;
+}
+
 int replaceFIFO(NumberLinkedList* list, int* array, int large){
     int min;
     min = 99999;
@@ -166,8 +200,7 @@ int replaceFIFO(NumberLinkedList* list, int* array, int large){
         if (aux < min) min = aux;
     }
 
-    return min;
-
+    return returnPositionArray(list, min, array, large);
 }
 
 int validateHit(int* numbers, int number, int large){
@@ -176,7 +209,7 @@ int validateHit(int* numbers, int number, int large){
     return 0;
 }
 
-void fifo(int blocks, NumberLinkedList* list){
+void fifo(int blocks, NumberLinkedList* list, ResultsList* results){
     int* numbers;
     numbers = listToArray(list);
 
@@ -189,6 +222,8 @@ void fifo(int blocks, NumberLinkedList* list){
         cacheOrganization = (int**)calloc(groups, sizeof(int*));
 
         for(int j=0; j<groups; j++) cacheOrganization[j] = (int*)calloc(i, sizeof(int));
+
+        for (int iaux=0; iaux<groups; iaux++) for(int jaux=0; jaux<i; jaux++) cacheOrganization[iaux][jaux] = -1;        
 
         for (int counter = 0; counter < list->length; counter++){
             int data = numbers[counter];
@@ -209,16 +244,47 @@ void fifo(int blocks, NumberLinkedList* list){
             }
         }
 
-        for (int iaux = 0; iaux<groups; iaux++){
-            for (int jaux = 0; jaux<i; jaux++){
-                printf(" %d ", cacheOrganization[iaux][jaux]);
+        appendResult(results, hits, misses, i, 1, cacheOrganization);
+
+    }
+}
+
+void lifo(int blocks, NumberLinkedList* list, ResultsList* results){
+    int* numbers;
+    numbers = listToArray(list);
+
+    for (int i=1; i<=blocks; i=i*2){
+        int** cacheOrganization;
+        int misses, hits, groups;
+        hits = 0;
+        misses = 0;
+        groups = blocks/i;
+        cacheOrganization = (int**)calloc(groups, sizeof(int*));
+
+        for(int j=0; j<groups; j++) cacheOrganization[j] = (int*)calloc(i, sizeof(int));
+
+        for (int iaux=0; iaux<groups; iaux++) for(int jaux=0; jaux<i; jaux++) cacheOrganization[iaux][jaux] = -1;
+
+        for (int counter = 0; counter < list->length; counter++){
+            int data = numbers[counter];
+            int position = data % groups;
+
+            int wordPosition;
+            wordPosition = searchSpace(cacheOrganization[position], i);
+
+            if (validateHit(cacheOrganization[position], data, i)){
+                hits++;
+            } else if (wordPosition == -1){
+                misses++;
+                wordPosition = i-1;
+                cacheOrganization[position][wordPosition] = data;
+            } else {
+                misses++;
+                cacheOrganization[position][wordPosition] = data;
             }
-            printf("\n");
         }
 
-        printf("hit: %d\nmiss: %d", hits, misses);
-
-        printf("\n");
+        appendResult(results, hits, misses, i, 2, cacheOrganization);
     }
 }
 
@@ -252,7 +318,27 @@ int main(int argc, char *argv[]){
     printGroups(blocksInCache);
     numbers = readFile(filename);
 
-    fifo(blocksInCache, numbers);
+    ResultsList* results;
+    results = createResults();
+
+    fifo(blocksInCache, numbers, results);
+    lifo(blocksInCache, numbers, results);
+
+    Results* aux;
+    aux = results->first;
+
+    while(aux){
+        if (aux->policy == 2 || 1){
+            for (int i=0; i<blocksInCache/aux->associativity; i++){
+                for (int j=0; j<aux->associativity; j++){
+                    printf(" %d ", aux->finalState[i][j]);
+                }
+                printf("\n");
+            }
+        }
+        printf("\n");
+        aux = aux->next;
+    }
 
     freeNumberList(numbers->first);
     free(filename);
